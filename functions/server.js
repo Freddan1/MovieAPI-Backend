c7d7d6811cd44653052d57d4706567dbd162fe56
Fredrik Eriksson <f.eriksson86@gmail.com>
2023-09-29 09:37:21 +0200

    :)

--- functions/server.js
+++ functions/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.use('/api/user',require('./routes/user_routes.js'));
+app.get('/', (req, res) => {
+   res.json({status: "ok"});
+});
 
 module.exports = app;
